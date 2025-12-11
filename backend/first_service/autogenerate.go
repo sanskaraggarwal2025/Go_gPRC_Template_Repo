89800ea4c9927f82f7e952e2f66db954b28e00ca
sanskaraggarwal2025 <aggarwalsanskar992@gmail.com>
2025-12-11 17:57:22 +0530

    corrected autogenerate go

--- backend/first_service/autogenerate.go
+++ backend/first_service/autogenerate.go
@@ -4,6 +4,6 @@ package main
 // This generates the go code from .proto files
 // For simple cases like this it avoids the need to have a Makefile
 
-//go:generate protoc --go_opt=module={{.ServiceModule}}/proto --go-grpc_opt=module={{..ServiceModule}}/proto --go_out=./proto/ --go-grpc_out=./proto/ --proto_path=../../proto service.proto
+//go:generate protoc --go_opt=module={{.ServiceModule}}/proto --go-grpc_opt=module={{.ServiceModule}}/proto --go_out=./proto/ --go-grpc_out=./proto/ --proto_path=../../proto service.proto
 
 // // go:generate sqlc -f db/scripts/sqlc.yaml generate
